09d0872c89a623bd4a2b6a197598ec985a436b19
TungX <xuantung.nguyen19@gmail.com>
2019-05-24 14:45:15 +0700

    Change port to 3000

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
 //    user_id: '5ce3c179d3886f34f9763ef3'
 //});
 
-var server = app.listen(80, function () {
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
